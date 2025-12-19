569b4a4d399140b86325274915212656220f03b4
Thomas Bellembois <thomas.bellembois@uca.fr>
2025-12-19 12:28:53 +0100

    Complete store_location handlers.

--- src/handlers/store_location.rs
+++ src/handlers/store_location.rs
@@ -1,6 +1,10 @@
-use axum::{Json, extract::State, http::HeaderMap};
+use axum::{
+    Json,
+    extract::{Query, State},
+    http::HeaderMap,
+};
 use chimitheque_types::{requestfilter::RequestFilter, storelocation::StoreLocation};
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 use crate::{AppState, errors::AppError, utils::get_chimitheque_person_id_from_headers};
 
@@ -19,14 +23,47 @@ pub async fn get_store_locations(
     let db_connection_pool = state.db_connection_pool.clone();
     let db_connection = db_connection_pool.get().unwrap();
 
-    let store_locations = chimitheque_db::storelocation::get_store_locations(
+    let mayerr_store_locations = chimitheque_db::storelocation::get_store_locations(
         db_connection.deref(),
         request_filter,
         chimitheque_person_id,
     );
 
-    match store_locations {
+    match mayerr_store_locations {
         Ok(store_locations) => Ok(Json(store_locations)),
+        Err(err) => Err(AppError::Database(err.to_string())),
+    }
+}
+
+pub async fn create_update_store_location(
+    State(state): State<AppState>,
+    Json(store_location): Json<StoreLocation>,
+) -> Result<Json<u64>, AppError> {
+    // Get the connection from the database.
+    let db_connection_pool = state.db_connection_pool.clone();
+    let mut db_connection = db_connection_pool.get().unwrap();
+
+    let mayerr_store_location_id = chimitheque_db::storelocation::create_update_store_location(
+        db_connection.deref_mut(),
+        store_location,
+    );
+
+    match mayerr_store_location_id {
+        Ok(store_location_id) => Ok(Json(store_location_id)),
+        Err(err) => Err(AppError::Database(err.to_string())),
+    }
+}
+
+pub async fn delete_store_location(
+    State(state): State<AppState>,
+    Query(id): Query<u64>,
+) -> Result<(), AppError> {
+    // Get the connection from the database.
+    let db_connection_pool = state.db_connection_pool.clone();
+    let mut db_connection = db_connection_pool.get().unwrap();
+
+    match chimitheque_db::storelocation::delete_store_location(db_connection.deref_mut(), id) {
+        Ok(_) => Ok(()),
         Err(err) => Err(AppError::Database(err.to_string())),
     }
 }
